4a4a8e99e4c3a4a909732271094ce46ce8e5049e
Elise Spain <elisespain@gmail.com>
2021-05-05 17:49:52 -0700

    Added a test nav bar for blog page

--- blog.js
+++ blog.js
@@ -1,43 +1,46 @@
-const blogDisplay = document.getElementById("app");
-const logo = document.createElement("img");
-const header = document.createElement("header");
-const container = document.createElement("div");
-const efpTitle = document.createElement("h1");
-// Card variables 
-//Header section
-logo.src = "/images/efp-final-logo.jpg";
-efpTitle.innerHTML = "Elite Financial Professionals Blog";
-header.append(logo, efpTitle);
-blogDisplay.append(header, container);
 
-//Container section
+let blogDisplay, header, container,logo,backArrow;
+blogDisplay = document.getElementById("app");
+header = document.createElement("header");
+container = document.createElement("div");
+logo = document.createElement("img");
+
+
+
+blogDisplay.append( container);
 container.setAttribute("class", "blog-container");
 
-//gain access to the medium api 
+
+
 fetch("https://v1.nocodeapi.com/espain/medium/qyYrQcocgvklnkmC")
 .then((response) => {
   return response.json()
 })
 .then((data) => {
   data.forEach((blog) => {
-    const blogCard = document.createElement("div");
-    const blogTitle = document.createElement("h1");
-    const blogAuthor = document.createElement("h2");
-    const blogDate = document.createElement("h3");
-    const blogBody = document.createElement("p");
-    const blogLink = document.createElement("a");
+    let blogCard, blogTitle, blogAuthor,blogDate,blogBody, authorInfo;
+    blogCard = document.createElement("article");
+    blogTitle = document.createElement("h1");
+    blogAuthor = document.createElement("h2");
+    blogDate = document.createElement("h3");
+    blogBody = document.createElement("p");
+    authorInfo = document.createElement("div");
     blogTitle.textContent = blog.title;
     blogAuthor.textContent = blog.creator;
     blogDate.textContent = blog.pubDate;
     blogBody.innerHTML = blog["content:encoded"];
-    blogLink.textContent = "Read More";
+    
 
     container.append(blogCard);
-    blogCard.append(blogTitle, blogAuthor, blogDate, blogBody, blogLink);
+    authorInfo.append(blogAuthor, blogDate);
+    blogCard.append(blogTitle, authorInfo, blogBody);
+    authorInfo.setAttribute("class", "block");
+    blogCard.setAttribute("class", "blog");
+    blogTitle.setAttribute("class", "blog__title");
+    blogAuthor.setAttribute("class", "block__author");
+    blogDate.setAttribute("class", "block__date");
+
+   
   })
   
 })
-//should display 1 article 
-
-//create a container element 
-//create a card element
